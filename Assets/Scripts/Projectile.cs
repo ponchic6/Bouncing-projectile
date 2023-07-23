9e1bbc4fb054d012d063f8b43a9b1c343c7744db
Aleksandr Stepanenko <alalstepan@gmail.com>
2023-07-23 14:34:47 +1200

    feat: исходный коммит

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    [Range (1, 1000)]
+    private float speed;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
